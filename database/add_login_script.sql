5575fa6cfabe84bb94d5af2eda70f42fb21f6c6a
Pao Cruz <A01562692@tec.mx>
2026-08-31 13:16:32 -0600

    Se modificaron los archivos y se añadió el script para crear el usuario en la DB

--- database/add_login_script.sql
+++ database/add_login_script.sql
@@ -2,6 +2,15 @@ CREATE LOGIN Dog_Atlas
 WITH PASSWORD = 'DogAtlas123';
 GO
 
+USE Perros_Atlas;
+GO
+
 CREATE USER Dog_Atlas
 FOR LOGIN Dog_Atlas;
+GO
+
+--Añadir como Datbase Owner
+ALTER ROLE db_owner
+ADD MEMBER Dog_Atlas;
+
 GO
